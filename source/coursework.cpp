98dd46097dc1d32f5ee7089fd33a211c7cfc6726
ScttRlph <23689704@stu.mmu.ac.uk>
2025-05-10 13:34:30 +0100

    init

---
+++ source/coursework.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <cmath>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <common/shader.hpp>
+#include <common/texture.hpp>
+#include <common/maths.hpp>
+#include <common/camera.hpp>
+#include <common/model.hpp>
+#include <common/light.hpp>
+
+// Function prototypes
+void keyboardInput(GLFWwindow *window);
+
+int main( void )
+{
+    // =========================================================================
+    // Window creation - you shouldn't need to change this code
+    // -------------------------------------------------------------------------
+    // Initialise GLFW
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        getchar();
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Open a window and create its OpenGL context
+    GLFWwindow* window;
+    window = glfwCreateWindow(1024, 768, "Computer Graphics Coursework", NULL, NULL);
+    
+    if( window == NULL ){
+        fprintf(stderr, "Failed to open GLFW window.\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    glewExperimental = true; // Needed for core profile
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    // -------------------------------------------------------------------------
+    // End of window creation
+    // =========================================================================
+    
+    //Enabling Depth Test;
+    glEnable(GL_DEPTH_TEST);
+
+    // Ensure we can capture keyboard inputs
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    const float vertices[] = {
+        // x     y     z
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+
+    static const unsigned int indices[] = {
+       0,1,2, //lower-right triagnle
+       0,2,3 //upper left triangle
+    };
+
+    unsigned int VAO;             // Vertex Buffer
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    const char* path = "../assets/MonitorScreen.jpg";
+    int width, height, nChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load(path, &width, &height, &nChannels, 0);
+
+    if (data)
+        std::cout << "Texture loaded." << std::endl;
+    else
+        std::cout << "Texture not loaded. Check the path." << std::endl;
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // Render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Get inputs
+        keyboardInput(window);
+        
+        // Clear the window
+        glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    // Close OpenGL window and terminate GLFW
+    glfwTerminate();
+    return 0;
+}
+
+void keyboardInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
